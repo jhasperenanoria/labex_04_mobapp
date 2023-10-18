c9ad3d879eeb7e56974abe23ab0872888ccdf25b
jhasperenanoria <82790991+jhasperenanoria@users.noreply.github.com>
2023-10-18 13:28:34 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace labex_04_mobApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveBlackCircle(LayoutOptions horizontalOptions, LayoutOptions verticalOptions)
+        {
+            blackCircle.HorizontalOptions = horizontalOptions;
+            blackCircle.VerticalOptions = verticalOptions;
+        }
+
+        private void OnVerticalStart(object sender, EventArgs e)
+        {
+            MoveBlackCircle(LayoutOptions.Start, blackCircle.VerticalOptions);
+        }
+
+        private void OnVerticalCenter(object sender, EventArgs e)
+        {
+            MoveBlackCircle(LayoutOptions.Start, LayoutOptions.Center);
+        }
+
+        private void OnVerticalEnd(object sender, EventArgs e)
+        {
+            MoveBlackCircle(LayoutOptions.Start, LayoutOptions.End);
+        }
+
+        private void OnHorizontalStart(object sender, EventArgs e)
+        {
+            MoveBlackCircle(LayoutOptions.Start, LayoutOptions.Center);
+        }
+
+        private void OnHorizontalCenter(object sender, EventArgs e)
+        {
+            MoveBlackCircle(LayoutOptions.Center, LayoutOptions.Center);
+        }
+
+        private void OnHorizontalEnd(object sender, EventArgs e)
+        {
+            MoveBlackCircle(LayoutOptions.End, LayoutOptions.Center);
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace labex_04_mobApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
+
